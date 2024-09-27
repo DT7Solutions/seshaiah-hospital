e0036fde6dcd8098f965f54020d8d7720fbf9283
website-krysta <website.krysta@gmail.com>
2024-09-27 17:09:55 +0530

    fixed issues

--- assets/js/script.js
+++ assets/js/script.js
@@ -586,4 +586,23 @@
 
 	
 
+	$('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:20,
+        nav:false,
+		smartSpeed: 500,
+		autoplay: 1000,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:2
+            },
+            1000:{
+                items:2
+            }
+        }
+    })
+
 })(window.jQuery);
