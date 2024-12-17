9cd00733d7be9e56fbdc1375252d72e9d14332f3
Chaitanya Kothakota <kcbgangadhar@gmail.com>
2024-12-17 13:33:17 +0530

    Form Submition Issues Fixed

--- assets/js/script.js
+++ assets/js/script.js
@@ -625,3 +625,47 @@
     })
 
 })(window.jQuery);
+
+
+// Contact Form Submition only
+// Contact Form
+document.getElementById("contact-form").addEventListener("submit", function(event) {
+    event.preventDefault(); // Prevent the default form submission
+
+    // Gather the form data
+    const username = document.getElementById("username").value;
+    const email = document.getElementById("contact-email").value;
+    const phone = document.getElementById("phone").value;
+    const subject = document.getElementById("subject").value;
+    const message = document.getElementById("message").value;
+
+    const emailData = {
+        from_name: username,
+        from_email: email,
+        phone: phone,
+        subject: subject,
+        message: message,
+    };
+
+    // EmailJS configuration
+    emailjs.init("YOUR_PUBLIC_KEY"); 
+    // Send appointment data to EmailJS
+    emailjs.send("YOUR_SERVICE_ID", "YOUR_TEMPLATE_ID", emailData)
+    .then(function (response) {
+        const successMessage = document.getElementById("contact-success-message");
+        successMessage.classList.remove("d-none");
+        console.log("Contact Form submitted successfully!", JSON.stringify(emailData, null, 2), response.status, response.text);
+        setTimeout(() => {
+            successMessage.classList.add("d-none");
+            document.getElementById("contact-form").reset();
+        }, 3000);
+    }, function (error) {
+        const errorMessage = document.getElementById("contact-error-message");
+        errorMessage.classList.remove("d-none");
+        console.log("Contact Form submission failed! Try Again!", JSON.stringify(emailData, null, 2), error);
+        setTimeout(() => {
+            errorMessage.classList.add("d-none");
+            document.getElementById("contact-form").reset();
+        }, 3000);
+    });
+});
